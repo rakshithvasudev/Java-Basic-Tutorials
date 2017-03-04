269fc9960d8cb2fc2a0832b6380e3b6297cf4de2
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-03-03 19:58:20 -0800

    Java Basic tutorials

---
+++ src/Car.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Rakshith on 3/3/2017.
+ */
+public class Car {
+    //mileage, price, doors count
+
+    double mileage;
+    int price;
+    int doors;
+
+
+
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+     Car benz = new Car();
+     benz.mileage = 10.54;
+     benz.price = 30000;
+     benz.doors = 4;
+
+        System.out.println("Benz has "+ benz.doors + " doors");
+        System.out.println("Benz is priced at "+ benz.price + " dollars");
+        System.out.println("The mileage being :" + benz.mileage);
+
+    }
+}
