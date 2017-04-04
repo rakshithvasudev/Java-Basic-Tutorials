2e5d203f4a7db0a0e4b1a6a64fab53a9a7f576c0
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-04-03 18:11:17 -0700

    Arrays in Java

--- src/arrays/LearningArrays.java
+++ src/arrays/LearningArrays.java
@@ -1,5 +1,7 @@
 package arrays;
 
+import java.util.Arrays;
+
 /**
  * Created by Rakshith on 4/3/2017.
  */
@@ -8,14 +10,22 @@ public class LearningArrays {
     public static void main(String[] args) {
 
         //Array is a continuous representation of same elements in the memory.
-        int[] studentMathScore = new int[5];
+        int[] studentMathScore = {51,65,21,30,45,76};
 
 
-        for (int i=0;i<5;i++){
-            studentMathScore[i]=i;
-        }
+//        for (int i=0;i<studentMathScore.length;i++){
+//            studentMathScore[i]=i;
+//        }
 
-        for (int i=0;i<5;i++)
-        System.out.println(studentMathScore[i]);
+
+        //1,2,3,8,9,40-> bin(3)=3
+        //System.out.println(Arrays.binarySearch(studentMathScore,8));
+
+
+       // System.out.println(Arrays.toString(studentMathScore));
+
+        Arrays.sort(studentMathScore);
+        System.out.println(Arrays.toString(studentMathScore));
+
     }
 }
