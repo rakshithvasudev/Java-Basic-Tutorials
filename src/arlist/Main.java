223e6734efa78d07d4c34bb13158f9a67e145688
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-04-13 23:13:55 -0700

    Iterator in Java

--- src/arlist/Main.java
+++ src/arlist/Main.java
@@ -1,6 +1,7 @@
 package arlist;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -9,18 +10,38 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
+
         List<Integer> integerList = new ArrayList<>();
+        Integer[] arrayVal = new Integer[5];
         integerList.add(5);
         integerList.add(35);
         integerList.add(75);
         integerList.add(105);
 
-        for (int currentInt:integerList) {
-            System.out.println("first: "+currentInt);
-        }
+//        for (int currentInt:integerList) {
+//            System.out.println("first: "+currentInt);
+//        }
+//
+//        for (int i=0;i<integerList.size();i++) {
+//            System.out.println(integerList.get(i));
+//        }
 
-        for (int i=0;i<integerList.size();i++) {
-            System.out.println(integerList.get(i));
-        }
+
+        // Part 2
+
+        //collections
+
+//        Iterator iterator = integerList.iterator();
+//        while(iterator.hasNext()){
+//            System.out.println("Next element is: "+ iterator.next());
+//        }
+//
+//        System.out.println(integerList.indexOf(75));
+//        System.out.println(integerList.contains(105));
+//        System.out.println(integerList.contains(20));
+
+
+
+
     }
 }
